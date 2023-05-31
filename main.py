40cc983cfa42908581482b65872dbe6b8b60c090
alexhatch <alexhatchh@gmail.com>
2023-05-31 11:38:54 -0700

    data added

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import pandas as pd
+
 def main():
     print("This is the main file for our project")
+
+    # a potential data set
+    # marijuana arrests in columbia
+    df = pd.read_csv("./data/Marijuana_Arrests.csv")
+    print(df)
 
 
 if __name__ == "__main__":
